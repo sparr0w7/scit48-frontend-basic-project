8d8ba1e5e3774ef8e0125a9df2cd843889a23488
jehwan <lluns67@gmail.com>
2025-11-08 16:25:21 +0900

    빠른 시작 부분 자바스크립트 작성

--- frontend/about/about.js
+++ frontend/about/about.js
@@ -1,1 +1,9 @@
-// TODO: 이 페이지 로직은 추후 구현
+const quickstartLinks = document.querySelectorAll('#quickstart + ul a');
+
+quickstartLinks.forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        window.location.href = `frontend/about/about.html/${this.textContent}`; // 주소 확인 후 수정필요
+    });
+});
