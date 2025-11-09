2f091cdb27f2de84d9f3474ef273dd8d13970d34
jehwan <lluns67@gmail.com>
2025-11-09 16:14:25 +0900

    about  쪽지쓰기 a 태그 링크 추가

--- frontend/about/about.js
+++ frontend/about/about.js
@@ -1,12 +1,4 @@
-const quickstartLinks = document.querySelectorAll('#quickstart + ul a');
 
-quickstartLinks.forEach(link => {
-    link.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        window.location.href = `frontend/about/about.html/${this.textContent}`; // 주소 확인 후 수정필요
-    });
-});
 document.querySelectorAll('.FAQ [role="button"]').forEach(button => {
     button.addEventListener('click', (e) => {
         const existing = document.querySelector('.faq-popup');
